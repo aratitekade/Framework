6c7296130c7ac449b325092a7cd9fe6c1d775bb3
arati <aratitekade4@gmail.com>
2025-04-16 15:32:44 +0530

    added f2 feature

--- src/main/java/com/qa/OrangeHRM/pages/FeaturePage.java
+++ src/main/java/com/qa/OrangeHRM/pages/FeaturePage.java
@@ -5,9 +5,12 @@ public class FeaturePage {
 	public static void main(String[] args) {
 		
 		//suppose this is functionality/feature which developer is releasing(and developer is you)
-		//if you want to give release version of this project
+		//if you want to give release version of this project so first we need to create tag(git tag v1.0)
 		
 		System.out.println("f1 feature");
+		
+		//suppose again after 10 days I need to add feature then add it.
+		System.out.println("f2 feture");
 
 	}
 
