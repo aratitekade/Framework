103cdc865363a1f97d90636b4cda968d38464761
arati <aratitekade4@gmail.com>
2025-04-16 15:27:19 +0530

    added f1 feature

---
+++ src/main/java/com/qa/OrangeHRM/pages/FeaturePage.java
@@ -0,0 +1,14 @@
+package com.qa.OrangeHRM.pages;
+
+public class FeaturePage {
+
+	public static void main(String[] args) {
+		
+		//suppose this is functionality/feature which developer is releasing(and developer is you)
+		//if you want to give release version of this project
+		
+		System.out.println("f1 feature");
+
+	}
+
+}
